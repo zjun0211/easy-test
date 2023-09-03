a25fe1fa580c4d73b7392bd7c6914d1a84b56ee3
zhj8678 <zhangjun2@noah-fund.com>
2023-09-03 21:36:55 +0800

    init

--- easy-web/src/views/index.vue
+++ easy-web/src/views/index.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-        <el-button type="primary" @click="loadData">新增用户</el-button>
+        <el-form :model="serchFormData" ref="formDataRef" label-width="80px" @submit.prevent>
+            <el-row :gutter="5">
+                <el-col :span="5">
+                    <el-form-item label="手机号">
+                        <el-input clearable placeholder="支持模糊搜索" v-model.trim="serchFormData.phoneFuzzy"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="5">
+                    <el-form-item label="用户名">
+                        <el-input clearable placeholder="支持模糊搜索" v-model.trim="serchFormData.userNameFuzzy"></el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="5">
+                    <el-button type="primary" @click="loadData">搜索</el-button>
+                    <el-button type="primary" @click="loadData">新增用户</el-button>
+                </el-col>
+            </el-row>
+        </el-form>
     </div>
     <el-table :data="tableData" style="width: 100%">
         <el-table-column label="用户ID" prop="id"></el-table-column>
@@ -19,26 +36,20 @@
         </el-table-column>
         <el-table-column label="角色" prop="roles">
             <template #default="scope">
-                {{ ROLES_MAP[scope.row.position] }}
+                {{ formateRoles(scope.row.roles) }}
             </template>
         </el-table-column>
         <el-table-column label="操作">
             <template #default="scope">
                 <el-button-group>
-                    <el-button 
-                    size="small" 
-                    type="primary"
-                    @click="handleEdit(scope.$index, scope.row)">修改</el-button>
-                    <el-button 
-                    size="small" 
-                    type="primary"
-                    @click="handleEdit(scope.$index, scope.row)">修改密码</el-button>
-                    <el-button 
-                    size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                    <el-button size="small" type="primary" @click="handleEdit(scope.$index, scope.row)">修改</el-button>
+                    <el-button size="small" type="primary" @click="handleEdit(scope.$index, scope.row)">修改密码</el-button>
+                    <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
                 </el-button-group>
             </template>
         </el-table-column>
     </el-table>
+    <el-pagination background layout="prev, pager, next" :total="tatal" :page-size="pageSize"/>
 </template>
   
 <script setup>
@@ -66,7 +77,26 @@ const ROLES_MAP = {
     3: "管理员"
 };
 
+const formateRoles = (roles) => {
+    if (typeof roles === 'string') {
+        let roleArray = roles.split(",");
+        let result = [];
+        roleArray.forEach(element => {
+            result.push(ROLES_MAP[element])
+        });
+        return result.join(",");
+    } else {
+        return "";
+    }
+}
+
+const serchFormData = reactive({
+    userName: "",
+});
+
 const tableData = ref([]);
+const tatal = ref(0);
+const pageSize = ref(0);
 
 const loadData = async () => {
     const result = await axios.post(api.loadDataList, {});
@@ -76,7 +106,9 @@ const loadData = async () => {
     //     ElMessage.error("获取数据失败");
     //     return;
     // }
-    tableData.value = data;
+    tableData.value = data.content;
+    tatal.value =  data.totalElements;
+    pageSize.value =  data.size;
 };
 
 loadData();
